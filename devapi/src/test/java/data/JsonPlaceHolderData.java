88a0e5960d3a4faae6ca2ece32a719c9cf1d7c48
Gokhan Agar <gokhanagar1@gmail.com>
2022-07-14 08:10:33 +0300

    V40

--- devapi/src/test/java/data/JsonPlaceHolderData.java
+++ devapi/src/test/java/data/JsonPlaceHolderData.java
@@ -25,4 +25,13 @@ public class JsonPlaceHolderData {
 
         return expectedData;
     }
+
+    public Map<String,Object> expectedDataWithAllKeys(Integer userId, String title, Boolean completed){
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId",userId);
+        expectedData.put("title", title);
+        expectedData.put("completed",completed);
+
+        return expectedData;
+    }
 }
