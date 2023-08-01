6a4f90c8acddce8918f2fb326853240cc41ffde0
gokhan agar <gokhanagar1@gmail.com>
2023-08-01 11:45:30 +0300

    V87

--- ApiProGoRest/src/test/java/stepDefinitions/jsonPlaceHolderStepDefinitions.java
+++ ApiProGoRest/src/test/java/stepDefinitions/jsonPlaceHolderStepDefinitions.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class jsonPlaceHolderStepDefinitions {
+
     Response response;
     JsonPath json;
 
